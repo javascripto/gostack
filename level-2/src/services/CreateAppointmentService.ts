5a1ccba8a87742359b01d6d75f4a7676085baff9
Yuri Almeida <yurialves2@gmail.com>
2020-06-30 00:49:55 -0300

    created Create AppointmentService

---
+++ level-2/src/services/CreateAppointmentService.ts
@@ -0,0 +1,43 @@
+import { startOfHour } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+/**
+ * [x] Recebe informações
+ * [x] Trata error/excessões
+ * [x] Acessa Repositório
+ */
+
+interface Request {
+  date: Date,
+  provider: string;
+}
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ date, provider }: Request): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentsRepository
+      .findByDate(appointmentDate);
+
+    if (findAppointmentInSameDate) {
+      throw new Error('This appoint is already booked');
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
