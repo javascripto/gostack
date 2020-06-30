529fb38186073abd3ff0f43b4856fee55b9ad46a
Yuri Almeida <yurialves2@gmail.com>
2020-06-30 00:50:34 -0300

    using repository service pattern

--- level-2/src/routes/appointments.routes.ts
+++ level-2/src/routes/appointments.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { startOfHour, parseISO } from 'date-fns';
+import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 
@@ -15,20 +16,18 @@ appointmentsRouter.get('/', (request, response) => {
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
-  const parsedDate = startOfHour(parseISO(date));
+  const parsedDate = parseISO(date);
 
-  const findAppointmentInSameDate = appointmentsRepository
-    .findByDate(parsedDate);
+  const createAppointment = new CreateAppointmentService(
+    appointmentsRepository,
+  );
 
-  if (findAppointmentInSameDate) {
-    return response
-      .status(400)
-      .json({ message: 'This appoint is already booked' });
+  try {
+    const appointment = createAppointment.execute({ provider, date: parsedDate });
+    return response.json(appointment);
+  } catch ({ message }) {
+    return response.status(400).json({ error: message });
   }
-
-  const appointment = appointmentsRepository.create(provider, parsedDate);
-
-  return response.json(appointment);
 });
 
 export default appointmentsRouter;
