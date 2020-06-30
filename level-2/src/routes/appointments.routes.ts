50c137a4619a05c79557c7f495251acd675135cd
Yuri Almeida <yurialves2@gmail.com>
2020-06-29 22:30:38 -0300

    created appointments router

--- level-2/src/routes/appointments.routes.ts
+++ level-2/src/routes/appointments.routes.ts
@@ -1,5 +1,19 @@
+import { uuid } from 'uuidv4';
 import { Router } from 'express';
 
 const appointmentsRouter = Router();
+
+const appointments = [];
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
+  appointments.push(appointment);
+  response.json(appointment);
+});
 
 export default appointmentsRouter;
--- level-2/src/routes/index.ts
+++ level-2/src/routes/index.ts
@@ -3,15 +3,7 @@ import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
-routes.use('/appointments', appointmentsRouter);
 
-routes.post('/', (request, response) => {
-  const { name, email } = request.body;
-  const user = {
-    name,
-    email,
-  };
-  return response.json(user);
-});
+routes.use('/appointments', appointmentsRouter);
 
 export default routes;
