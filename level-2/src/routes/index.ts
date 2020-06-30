909568c52b79c58491d552d2a4839ef08853a67e
Yuri Almeida <yurialves2@gmail.com>
2020-06-29 22:03:00 -0300

    config eslint, editorconfig, prettier

---
+++ level-2/src/routes/index.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+
+import appointmentsRouter from './appointments.routes';
+
+const routes = Router();
+routes.use('/appointments', appointmentsRouter);
+
+routes.post('/', (request, response) => {
+  const { name, email } = request.body;
+  const user = {
+    name,
+    email,
+  };
+  return response.json(user);
+});
+
+export default routes;
