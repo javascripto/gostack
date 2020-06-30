e9267d6e9b9abbe35102b517563d216230cde460
Yuri Almeida <yurialves2@gmail.com>
2020-06-29 23:18:02 -0300

    using AppointmentsRepository

--- level-2/src/routes/appointments.routes.ts
+++ level-2/src/routes/appointments.routes.ts
@@ -1,19 +1,29 @@
-import { uuid } from 'uuidv4';
 import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
-  const appointment = {
-    id: uuid(),
-    provider,
-    date,
-  };
-  appointments.push(appointment);
-  response.json(appointment);
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentsRepository
+    .findByDate(parsedDate);
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appoint is already booked' });
+  }
+
+  const appointment = appointmentsRepository.create(provider, parsedDate);
+
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
