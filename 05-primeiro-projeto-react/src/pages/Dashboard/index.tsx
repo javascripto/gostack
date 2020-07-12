0f0868924bbea71ceb025f22d0ef1320d1e3d08c
Yuri Almeida <yurialves2@gmail.com>
2020-07-12 01:45:29 -0300

    form erro and styled props

--- 05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ 05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -4,7 +4,7 @@ import React, { useState, FormEvent } from 'react';
 
 import api from '../../services/api';
 import logoImage from '../../assets/logo.svg';
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
 
 interface Repository {
   full_name: string;
@@ -17,19 +17,29 @@ interface Repository {
 
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
   const [repositories, setRepositories] = useState<Repository[]>([]);
   async function handleAddRepository(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    const response = await api.get<Repository>(`/repos/${newRepo}`);
-    setRepositories([...repositories, response.data]);
-    setNewRepo('');
+    if (!newRepo.trim()) {
+      setInputError('Digite o autor/nome do repositório');
+      return;
+    }
+    try {
+      const response = await api.get<Repository>(`/repos/${newRepo}`);
+      setRepositories([...repositories, response.data]);
+      setNewRepo('');
+      setInputError('');
+    } catch (error) {
+      setInputError('Erro na busca por esse repositório');
+    }
   }
   return (
     <>
-      <img src={logoImage} alt="" />
+      <img src={logoImage} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form onSubmit={handleAddRepository}>
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
         <input
           value={newRepo}
           onChange={e => setNewRepo(e.target.value)}
@@ -37,6 +47,7 @@ const Dashboard: React.FC = () => {
         />
         <button type="submit">Pesquisar</button>
       </Form>
+      {inputError && <Error>{inputError}</Error>}
 
       <Repositories>
         {repositories.map(({ owner, full_name, description }) => (
