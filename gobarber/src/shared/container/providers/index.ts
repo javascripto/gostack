8dab10698098a180ee7ef3f0a1891625768f0716
Yuri Almeida <yurialves2@gmail.com>
2020-09-24 08:18:11 -0300

    created mail prvider iplementation

--- gobarber/src/shared/container/providers/index.ts
+++ gobarber/src/shared/container/providers/index.ts
@@ -1,9 +1,16 @@
 import { container } from 'tsyringe';
 
+import IMailProvider from './MailProvider/models/IMailProvider';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
+import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
+);
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  new EtherealMailProvider(),
 );
